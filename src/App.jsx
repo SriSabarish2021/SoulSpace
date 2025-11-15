c01dfcaaac23433c30b768a2d525f77de45c3780
SriSabarish2021 <srisabarish003@gmail.com>
2025-11-15 23:03:39 +0530

    first-of soulspce

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Nav from './Components/NavBar/Nav'
+
+import './App.scss'
+
+function App() {
+
+  return (
+    <div className='website-main-container'>
+      <Nav></Nav>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/NavBar/Nav.jsx
@@ -0,0 +1,53 @@
+import '../../Sass/NavBar/Nav.scss';
+import { FaSearch } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import { FaWhatsapp } from "react-icons/fa";
+import { FaAngleRight } from "react-icons/fa6";
+
+const Nav = () => {
+  return (
+    <div className="nav-bar-for-website">
+      <div className='nav-container-for-search'>
+        <div className="search-bar-nav">
+          <div className='search-bar-container-in-nav'>
+            <input type="text" placeholder='Your Needs...' className='input-for-search'/>
+
+          </div>
+          <div className="search-icon-holder-nav">
+            <FaSearch className='search-icon-nav'/>
+          </div>
+        </div>
+        <div className="social-link-nav">
+          <div className="social-icons-nav">
+            <FaFacebook className='social-icon-nav'/>
+          </div>
+          <div className="social-icons-nav">
+            <FaInstagram className='social-icon-nav'/>
+          </div>
+          <div className="social-icons-nav">
+            <FaTwitter className='social-icon-nav'/>
+          </div>
+          <div className="social-icons-nav">
+            <FaWhatsapp className='social-icon-nav'/>
+          </div>
+        </div>
+      </div>
+      <div className="nav-container-for-main-navbar">
+        <div className="site-logo">
+          <img src="./image-removebg-preview.webp" alt="" className='site-logo-img'/>
+          <p className='site-logo-name'>SOULspace</p>
+        </div>
+        <div className="nav-sections">
+           <a href='#' className='nav-link-para'>Home</a>
+            <a href='#' className='nav-link-para'>About us</a>
+             <a href='#' className='nav-link-para'>Programs <FaAngleRight className='arrow-change-icon'/></a>
+              <a href='#' className='nav-link-para'>Contact</a>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Nav
