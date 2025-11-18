71f2c083df3f699f26df6e7d79464b3cd271fbf6
SriSabarish2021 <srisabarish003@gmail.com>
2025-11-18 19:29:43 +0530

    qualification

--- src/Components/WebsiteComponents/HomepageAll.jsx
+++ src/Components/WebsiteComponents/HomepageAll.jsx
@@ -1,11 +1,13 @@
 import HomeMain from '../HomePageComponents/MainHome/HomeMain.jsx';
 import Programs from '../HomePageComponents/Programs/Programs.jsx';
+import Qualification from '../HomePageComponents/Qualification/Qualification';
 
 const HomepageAll = () => {
   return (
     <>
       <HomeMain/>
       <Programs/>
+      <Qualification/>
       
     </>
   )
